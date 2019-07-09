bb28d54e5cee565a7dc694b5790e40dcc24dd7d7
Charles-Éric Vallée <snokilo@hotmail.com>
2019-07-09 15:24:42 -0400

    working on upload funtion

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 exports.printMsg = function() {
     console.log("This is a message from the Ago-DO-Tool package");
 };
+exports.upload = (/** Put here params needed in this function */) => {
+    console.log("Work still in progress");
+    /** 
+     * Put here code to upload file to digital ocean droplets
+     */
+}
